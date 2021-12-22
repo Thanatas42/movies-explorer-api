8f8551fe0d28655f060dd9078772bd6de86381a8
Dmitry Dudin <dmitrodonskoy@yandex.ru>
2021-12-22 19:20:36 +0300

    Ревью 3

--- controllers/users.js
+++ controllers/users.js
@@ -45,33 +45,31 @@ const editUser = (req, res, next) => modelUser.findByIdAndUpdate(
     }
   });
 
-const createUser = (req, res, next) => bcrypt.hash(req.body.password, salt)
-  .then((hash) => {
-    const {
-      name, email,
-    } = req.body;
-    return modelUser.create({
+const createUser = (req, res, next) => {
+  const {
+    name, email,
+  } = req.body;
+  bcrypt.hash(req.body.password, salt)
+    .then((hash) => modelUser.create({
       name, email, password: hash,
-    }).catch((err) => {
-      next(err);
-    })
-      .then((user) => res.send({
-        user: {
-          _id: user._id,
-          email: user.email,
-          name: user.name,
-        },
-      }))
-      .catch((err) => {
-        if (err.name === 'ValidationError') {
-          next(new BadRequestErr('Переданы некорректные данные при создании пользователя'));
-        } else if (err.code === 11000) {
-          next(new DoubleErr('Пользователь с такой электронной почтой уже зарегистрирован'));
-        } else {
-          next(err);
-        }
-      });
-  });
+    }))
+    .then((user) => res.send({
+      user: {
+        _id: user._id,
+        email: user.email,
+        name: user.name,
+      },
+    }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new BadRequestErr('Переданы некорректные данные при создании пользователя'));
+      } else if (err.code === 11000) {
+        next(new DoubleErr('Пользователь с такой электронной почтой уже зарегистрирован'));
+      } else {
+        next(err);
+      }
+    });
+};
 
 const login = (req, res, next) => {
   const { email, password } = req.body;
